7a7337def4405f7343c200ee24d918f9a1434bea
DevShaded <devshaded@devshaded.com>
2022-06-12 14:25:29 +0200

    Added dynamic meta tags for seo

--- resources/js/Layouts/DefaultLayout.vue
+++ resources/js/Layouts/DefaultLayout.vue
@@ -3,9 +3,33 @@ import Navbar from '@/Layouts/Navbar';
 import Footer from "@/Components/Footer";
 import SvgWaveDown from '@/Components/SvgWaveDown'
 import SvgWaveUp from '@/Components/SvgWaveUp'
+import { Head } from "@inertiajs/inertia-vue3";
+
+defineProps({
+    title:   String,
+    content: String,
+    url:     String
+})
 </script>
 
 <template>
+    <Head>
+        <title>{{ title }} - DevShaded</title>
+        <meta name="description" :content="content">
+
+        <meta property="og:url" :content="`https://devshaded.com${url}`">
+        <meta property="og:title" :content="`${title} - DevShaded`">
+        <meta property="og:description" :content="content">
+        <meta property="og:image" content="https://devshaded.com/images/logo.svg">
+
+        <meta name="twitter:domain" content="https://devshaded.com">
+        <meta name="twitter:card" content="summary">
+        <meta name="twitter:url" :content="`https://devshaded.com${url}`">
+        <meta name="twitter:title" :content="`${title} - DevShaded`">
+        <meta name="twitter:description" :content="content">
+        <meta name="twitter:image" content="https://devshaded.com/images/logo.svg">
+    </Head>
+
     <div class="min-h-screen bg-gray-900">
         <!-- Navbar -->
         <Navbar />
