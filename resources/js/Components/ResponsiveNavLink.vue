cd307d0efc189856caae6453e56b6cadba6eec4f
DevShaded <devshaded@devshaded.com>
2022-06-12 13:46:13 +0200

    Added a default app layout for the website

---
+++ resources/js/Components/ResponsiveNavLink.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { Link } from "@inertiajs/inertia-vue3";
+import { computed } from "vue";
+
+const props = defineProps(['href', 'active']);
+
+const classes = computed(() => props.active
+    ? 'bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium'
+    : 'text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'
+);
+</script>
+
+<template>
+    <Link :href="href" :class="classes">
+        <slot />
+    </Link>
+</template>
