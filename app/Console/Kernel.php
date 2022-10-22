8a6c9ffe2f0d47890a6113b9ed67fcf6097d6ba0
DevShaded <devshaded@devshaded.com>
2022-10-22 13:44:06 +0200

    Added artisan command for updating the GitHub repos for the projects page && added a schedule for that command to run daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,9 +13,10 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
+    protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('update:github-repos')->daily();
     }
 
     /**
